a1b6c51386d7c535a65fe68bf574d7ee601f7b73
negihimanshu015 <125742145+negihimanshu015@users.noreply.github.com>
2024-07-26 10:30:33 +0530

    init

---
+++ TabGuardian/script.js
@@ -0,0 +1,18 @@
+//Element
+const recover= document.getElementById("Recover")
+const startbutton= document.getElementById("Start")
+const stopbutton= document.getElementById("Stop")
+
+
+
+recover.onclick= () =>{
+    console.log("hello world");
+}
+
+start.onclick= () =>{
+    console.log("hello world");
+}
+
+stopbutton.onclick= () =>{
+    console.log("hello world");
+}
