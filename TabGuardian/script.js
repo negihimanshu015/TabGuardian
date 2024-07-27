cdaebbc940760caab4a99347b7fc094048267784
negihimanshu015 <125742145+negihimanshu015@users.noreply.github.com>
2024-07-27 20:45:39 +0530

    working correctly

--- TabGuardian/script.js
+++ TabGuardian/script.js
@@ -3,31 +3,7 @@ const startButton = document.getElementById("Start");
 const stopButton = document.getElementById("Stop");
 
 recover.onclick = () => {
-    chrome.storage.local.get("tabdata", async (result) => {
-        const tabdata = result.tabdata || [];
-        if (tabdata.length > 0) {
-            const windowMap = new Map();            
-            
-            for (let tab of tabdata) {
-                if (!windowMap.has(tab.windowId)) {
-                    const win = await chrome.windows.create({ url: "about:blank" });
-                    windowMap.set(tab.windowId, win.id);
-                }
-            }
-           
-            for (let tab of tabdata) {
-                const winId = windowMap.get(tab.windowId);
-                chrome.tabs.create({ windowId: winId, url: tab.url });
-            }
-            
-            for (let winId of windowMap.values()) {
-                const tabs = await chrome.tabs.query({ windowId: winId });
-                if (tabs.length > 0 && tabs[0].url === "about:blank") {
-                    chrome.tabs.remove(tabs[0].id);
-                }
-            }
-        }
-    });
+    restore();    
 };
 
 startButton.onclick = () => {
@@ -37,3 +13,60 @@ startButton.onclick = () => {
 stopButton.onclick = () => {
     console.log('Stop button clicked');    
 };
+
+
+
+let restore = async () => {
+    const result = await chrome.storage.local.get("tabdata");
+    const storedTabs = result.tabdata || [];
+  
+    // Filter out duplicates based on URL
+    const tabData = storedTabs.reduce((acc, tab) => {
+      const existingIndex = acc.findIndex(existingTab => existingTab.url === tab.url);
+      if (existingIndex === -1) {
+        acc.push(tab);
+      } else {
+        // Prioritize the tab with windowId
+        if (tab.windowId && !acc[existingIndex].windowId) {
+          acc[existingIndex] = tab;
+        }
+      }
+      return acc;
+    }, []);
+  
+    createWindowsAndTabs(tabData);
+
+    
+  };
+  
+
+
+  function createWindowsAndTabs(tabData) {
+    // Group tabs by windowId
+    const tabsByWindowId = Object.values(tabData).reduce((acc, tab) => {
+      const windowId = tab.windowId || chrome.windows.WINDOW_ID_CURRENT;
+      if (!acc[windowId]) {
+        acc[windowId] = [];
+      }
+      acc[windowId].push(tab);
+      return acc;
+    }, {});
+  
+    // Create windows and tabs
+    for (const windowId in tabsByWindowId) {
+      const tabs = tabsByWindowId[windowId];
+      if (windowId !== chrome.windows.WINDOW_ID_CURRENT) {
+        // Create a new window if it doesn't exist
+        chrome.windows.create({ focused: true }, (newWindow) => {
+          tabs.forEach(tab => {
+            chrome.tabs.create({ url: tab.url, windowId: newWindow.id });
+          });
+        });
+      } else {
+        // Create tabs in the current window
+        tabs.forEach(tab => {
+          chrome.tabs.create({ url: tab.url, windowId });
+        });
+      }
+    }
+  }
--- background.js
+++ background.js
@@ -2,58 +2,62 @@ let tabdata = [];
 
 chrome.storage.local.get('tabdata' , (result) => {
   tabdata = result.tabData || [];
+});
 
-  chrome.tabs.query({
-    url: [
-        "http://*/*",
-        "https://*/*"
-    ]
-  })
+chrome.tabs.query({
+  url: [
+      "http://*/*",
+      "https://*/*"
+  ]
+})
 
 .then(tabs => {
-    tabdata = tabdata.concat(tabs.map(tab =>({ url:tab.url, id: tab.id })));
-
-    chrome.windows.getAll({ populate: true }).then(windows => {
-      windows.forEach(win => {
-        win.tabs.forEach(tab => {
-          tabdata.push({ url: tab.url, id: tab.id, windowId: win.id });
-        });
-      });
-      chrome.storage.local.set({ tabdata });
-    });  
+    tabdata = tabdata.concat(tabs.map(tab =>({ url:tab.url, id: tab.id, windowId: tab.windowId })));      
     
     //on updating current tab
-    chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-      if (changeInfo.url) {
-        const index = tabdata.findIndex(item => item.id === tabId);
-        if (index !== -1) {
-          tabdata[index].url = changeInfo.url;          
-        }
-      }
-    });
+    update_tab();
     
     //pushing newly opened tab.
-    chrome.tabs.onCreated.addListener((tab) => {      
-        tabdata.push({ url: tab.url, id: tab.id });
-        chrome.storage.local.set({ tabdata });                
-        }       
-      )
+    push_tab();
     
-    //Removing closed tabs from array.
-    
-    chrome.tabs.onRemoved.addListener(
-        (tabId) => { 
-            const remIndex = tabdata.findIndex(item => item.id === tabId);
-
-            if (remIndex !== -1) { // Check if tab found
-                tabdata.splice(remIndex, 1); // Remove object at the index 
-                chrome.storage.local.set({ tabdata });              
-              }                    
-            
-        }
-    ) 
-    
+    //Removing closed tabs from array.    
+    remove_tab();
+    console.log(tabdata);
+       
     }
 	
    )
-}) 
+
+
+let update_tab = () => {
+  chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+    if (changeInfo.url) {
+      const index = tabdata.findIndex(item => item.id === tabId);
+      if (index !== -1) {
+        tabdata[index].url = changeInfo.url;          
+      }
+    }
+  });
+}
+
+let push_tab = () => {
+  chrome.tabs.onCreated.addListener((tab) => {      
+    tabdata.push({ url: tab.url, id: tab.id });
+    chrome.storage.local.set({ tabdata });                
+    }       
+  )
+}
+
+let remove_tab =() => {
+  chrome.tabs.onRemoved.addListener(
+    (tabId) => { 
+        const remIndex = tabdata.findIndex(item => item.id === tabId);
+
+        if (remIndex !== -1) { 
+            tabdata.splice(remIndex, 1); 
+            chrome.storage.local.set({ tabdata });              
+          }                    
+        
+    }
+)
+}
